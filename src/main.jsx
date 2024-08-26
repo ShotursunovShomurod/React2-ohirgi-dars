d573255276de67e2b3c8790adea154070db73f65
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-08-26 10:07:07 +0500

    togirlolmadim

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./components/context/Index.jsx";
-import { initialState, reducer } from "./components/context/Reduc.js";
+import { initialState, reducer } from './components/context/Reduc.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
